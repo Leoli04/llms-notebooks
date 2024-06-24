d3abd7ab379a3aa17235bfdc37a75327bde05986
LeoLi04 <leoli_4@163.com>
2024-06-24 09:48:56 +0800

    feat：注意力机制

---
+++ d2l/code/rnn/NWKernel_param.py
@@ -0,0 +1,81 @@
+
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def f(x):
+    return 2 * torch.sin(x) + x ** 0.8
+
+def plot_kernel_reg(y_hat):
+    d2l.plot(x_test, [y_truth, y_hat], 'x', 'y', legend=['Truth', 'Pred'],
+             xlim=[0, 5], ylim=[-1, 5])
+    d2l.plt.plot(x_train, y_train, 'o', alpha=0.5);
+
+'''
+定义Nadaraya-Watson核回归的带参数版本
+'''
+class NWKernelRegression(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.w = nn.Parameter(torch.rand((1,), requires_grad=True))
+
+    def forward(self, queries, keys, values):
+        # queries和attention_weights的形状为(查询个数，“键－值”对个数)
+        queries = queries.repeat_interleave(keys.shape[1]).reshape((-1, keys.shape[1]))
+        self.attention_weights = nn.functional.softmax(
+            -((queries - keys) * self.w)**2 / 2, dim=1)
+        # values的形状为(查询个数，“键－值”对个数)
+        # weights.unsqueeze(1) 会在weights张量的第二个维度上增加一个大小为1的维度。-1 代表最后一个维度
+        return torch.bmm(self.attention_weights.unsqueeze(1),
+                         values.unsqueeze(-1)).reshape(-1)
+
+
+if __name__ == '__main__':
+
+    # 生成数据
+    n_train = 50  # 训练样本数
+    x_train, _ = torch.sort(torch.rand(n_train) * 5)  # 排序后的训练样本
+
+
+    y_train = f(x_train) + torch.normal(0.0, 0.5, (n_train,))  # 训练样本的输出
+    x_test = torch.arange(0, 5, 0.1)  # 测试样本
+    y_truth = f(x_test)  # 测试样本的真实输出
+    n_test = len(x_test)  # 测试样本数
+
+    # 将训练数据集变换为键和值用于训练注意力模型
+
+    # X_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输入
+    X_tile = x_train.repeat((n_train, 1))
+    # Y_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输出
+    Y_tile = y_train.repeat((n_train, 1))
+    # keys的形状:('n_train'，'n_train'-1)
+    keys = X_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+    # values的形状:('n_train'，'n_train'-1)
+    values = Y_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+
+    # 训练带参数的注意力汇聚模型时，使用平方损失函数和随机梯度下降。
+
+    net = NWKernelRegression()
+    loss = nn.MSELoss(reduction='none')
+    trainer = torch.optim.SGD(net.parameters(), lr=0.5)
+    animator = d2l.Animator(xlabel='epoch', ylabel='loss', xlim=[1, 5])
+
+    for epoch in range(5):
+        trainer.zero_grad()
+        l = loss(net(x_train, keys, values), y_train)
+        l.sum().backward()
+        trainer.step()
+        print(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
+        animator.add(epoch + 1, float(l.sum()))
+
+    # keys的形状:(n_test，n_train)，每一行包含着相同的训练输入（例如，相同的键）
+    keys = x_train.repeat((n_test, 1))
+    # value的形状:(n_test，n_train)
+    values = y_train.repeat((n_test, 1))
+    y_hat = net(x_test, keys, values).unsqueeze(1).detach()
+    plot_kernel_reg(y_hat)
+
+    d2l.show_heatmaps(net.attention_weights.unsqueeze(0).unsqueeze(0),
+                      xlabel='Sorted training inputs',
+                      ylabel='Sorted testing inputs')

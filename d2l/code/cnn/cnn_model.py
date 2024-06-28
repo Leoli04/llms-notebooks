ee9ab648d5a9e69e9ad399ae357903d6eed64c96
LeoLi04 <leoli_4@163.com>
2024-06-28 18:30:40 +0800

    feat：批量规范化

--- d2l/code/cnn/cnn_model.py
+++ d2l/code/cnn/cnn_model.py
@@ -1,8 +1,13 @@
 
+import torch
 from torch import nn
+from torch.nn import functional as F
+
 
 def lenet():
     '''
+    LeNet是最早发布的卷积神经网络之一
+
     LeNet（LeNet-5）由两个部分组成：
         卷积编码器：由两个卷积层组成;
         全连接层密集块：由三个全连接层组成。
@@ -20,6 +25,10 @@ def lenet():
     return net
 
 def  alexnet():
+    '''
+    alexnet给人的启发：从浅层网络到深层网络，
+    :return:
+    '''
     net = nn.Sequential(
         # 这里使用一个11*11的更大窗口来捕捉对象。
         # 同时，步幅为4，以减少输出的高度和宽度。
@@ -45,3 +54,223 @@ def  alexnet():
         # 最后是输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
         nn.Linear(4096, 10))
     return net
+
+def vgg_block(num_convs, in_channels, out_channels):
+    '''
+    VGG块：使用了带有3 X 3卷积核（有多个）、填充为1（保持高度和宽度）的卷积层，和带有2 X 2汇聚窗口、步幅为2（每个块后的分辨率减半）的最大汇聚层。
+    :param num_convs: 卷积层的数量
+    :param in_channels: 输入通道的数量
+    :param out_channels: 输出通道的数量
+    :return:
+    '''
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.Conv2d(in_channels, out_channels,
+                                kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+        in_channels = out_channels
+    layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+    return nn.Sequential(*layers)
+
+def vgg(conv_arch):
+    '''
+    VGG-11
+    原始VGG网络有5个卷积块，其中前两个块各有一个卷积层，后三个块各包含两个卷积层。 第一个模块有64个输出通道，
+    每个后续模块将输出通道数量翻倍，直到该数字达到512。由于该网络使用8个卷积层和3个全连接层，因此它通常被称为VGG-11。
+
+    LeNet、AlexNet和VGG都有一个共同的设计模式：通过一系列的卷积层与汇聚层来提取空间结构特征；
+
+    给人的启发：可以设计块组合使用
+
+    :param conv_arch: 每个VGG块里卷积层个数和输出通道数，
+    如：conv_arch = ((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))
+    :return:
+    '''
+    conv_blks = []
+    in_channels = 1
+    # 卷积层部分
+    for (num_convs, out_channels) in conv_arch:
+        conv_blks.append(vgg_block(num_convs, in_channels, out_channels))
+        in_channels = out_channels
+
+    return nn.Sequential(
+        *conv_blks, nn.Flatten(),
+        # 全连接层部分
+        nn.Linear(out_channels * 7 * 7, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 10))
+
+
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    '''
+     NiN块以一个普通卷积层开始，后面是两个1 X 1的卷积层。这两个 1 X 1卷积层充当带有ReLU激活函数的逐像素全连接层。
+     第一层的卷积窗口形状通常由用户设置。 随后的卷积窗口形状固定为 1 X 1
+    :param in_channels: 输入通道
+    :param out_channels: 输出通道
+    :param kernel_size: 第一层的卷积窗口形状
+    :param strides: 第一层的卷积窗口步幅
+    :param padding: 第一层的卷积窗口填充
+    :return:
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
+
+def nin():
+    '''
+     NiN使用窗口形状为 11 X 11、5 X 5、3 X 3的卷积层，输出通道数量与AlexNet中的相同。 每个NiN块后有一个最大汇聚层，
+     汇聚窗口形状为3 X 3，步幅为2。
+
+    NiN和AlexNet之间的一个显著区别是NiN去除了容易造成过拟合的全连接层，将它们替换为全局平均汇聚层（即在所有位置上进行求和）。
+    该汇聚层通道数量为所需的输出数量
+
+    NiN使用一个NiN块，其输出通道数等于标签类别的数量。最后放一个全局平均汇聚层（global average pooling layer），
+    生成一个对数几率 （logits）。NiN设计的一个优点是，它显著减少了模型所需参数的数量。但是，这种设计有时会增加训练模型的时间。
+    :return:
+    '''
+    net = nn.Sequential(
+        nin_block(1, 96, kernel_size=11, strides=4, padding=0),
+        nn.MaxPool2d(3, stride=2),
+        nin_block(96, 256, kernel_size=5, strides=1, padding=2),
+        nn.MaxPool2d(3, stride=2),
+        nin_block(256, 384, kernel_size=3, strides=1, padding=1),
+        nn.MaxPool2d(3, stride=2),
+        nn.Dropout(0.5),
+        # 标签类别数是10
+        nin_block(384, 10, kernel_size=3, strides=1, padding=1),
+        nn.AdaptiveAvgPool2d((1, 1)),
+        # 将四维的输出转成二维的输出，其形状为(批量大小,10)
+        nn.Flatten())
+    return net
+
+class Inception(nn.Module):
+
+    '''
+    Inception块由四条并行路径组成。 前三条路径使用窗口大小为 1 X 1、3 X 3、5 X 5的卷积层，从不同空间大小中提取信息。
+    中间的两条路径在输入上执行1 X 1卷积，以减少通道数，从而降低模型的复杂性。 第四条路径使用3 X 3最大汇聚层，然后使用
+    1 X 1卷积层来改变通道数。
+    '''
+
+    # c1--c4是每条路径的输出通道数
+    def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):
+        super(Inception, self).__init__(**kwargs)
+        # 线路1，单1x1卷积层
+        self.p1_1 = nn.Conv2d(in_channels, c1, kernel_size=1)
+        # 线路2，1x1卷积层后接3x3卷积层
+        self.p2_1 = nn.Conv2d(in_channels, c2[0], kernel_size=1)
+        self.p2_2 = nn.Conv2d(c2[0], c2[1], kernel_size=3, padding=1)
+        # 线路3，1x1卷积层后接5x5卷积层
+        self.p3_1 = nn.Conv2d(in_channels, c3[0], kernel_size=1)
+        self.p3_2 = nn.Conv2d(c3[0], c3[1], kernel_size=5, padding=2)
+        # 线路4，3x3最大汇聚层后接1x1卷积层
+        self.p4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.p4_2 = nn.Conv2d(in_channels, c4, kernel_size=1)
+
+    def forward(self, x):
+        p1 = F.relu(self.p1_1(x))
+        p2 = F.relu(self.p2_2(F.relu(self.p2_1(x))))
+        p3 = F.relu(self.p3_2(F.relu(self.p3_1(x))))
+        p4 = F.relu(self.p4_2(self.p4_1(x)))
+        # 在通道维度上连结输出
+        return torch.cat((p1, p2, p3, p4), dim=1)
+
+def googlenet():
+    # 第一个模块使用64个通道、7 X 7卷积层。
+    b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                       nn.ReLU(),
+                       nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第二个模块使用两个卷积层：第一个卷积层是64个通道、1 X 1卷积层；第二个卷积层使用将通道数量增加三倍的3 X 3卷积层
+    b2 = nn.Sequential(nn.Conv2d(64, 64, kernel_size=1),
+                       nn.ReLU(),
+                       nn.Conv2d(64, 192, kernel_size=3, padding=1),
+                       nn.ReLU(),
+                       nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第三个模块串联两个完整的Inception块。 第一个Inception块的输出通道数为64+128+32+32=256
+    # 第二个Inception块的输出通道数增加到 128+182+94+64=480
+    b3 = nn.Sequential(Inception(192, 64, (96, 128), (16, 32), 32),
+                       Inception(256, 128, (128, 192), (32, 96), 64),
+                       nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+    # 第四模块串联了5个Inception块
+    b4 = nn.Sequential(Inception(480, 192, (96, 208), (16, 48), 64),
+                       Inception(512, 160, (112, 224), (24, 64), 64),
+                       Inception(512, 128, (128, 256), (24, 64), 64),
+                       Inception(512, 112, (144, 288), (32, 64), 64),
+                       Inception(528, 256, (160, 320), (32, 128), 128),
+                       nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+    b5 = nn.Sequential(Inception(832, 256, (160, 320), (32, 128), 128),
+                       Inception(832, 384, (192, 384), (48, 128), 128),
+                       nn.AdaptiveAvgPool2d((1, 1)),
+                       nn.Flatten())
+
+    net = nn.Sequential(b1, b2, b3, b4, b5, nn.Linear(1024, 10))
+    return net
+
+class BatchNorm(nn.Module):
+    '''
+    批量规范化层
+    '''
+    # num_features：完全连接层的输出数量或卷积层的输出通道数。
+    # num_dims：2表示完全连接层，4表示卷积层
+    def __init__(self, num_features, num_dims):
+        super().__init__()
+        if num_dims == 2:
+            shape = (1, num_features)
+        else:
+            shape = (1, num_features, 1, 1)
+        # 参与求梯度和迭代的拉伸和偏移参数，分别初始化成1和0
+        self.gamma = nn.Parameter(torch.ones(shape))
+        self.beta = nn.Parameter(torch.zeros(shape))
+        # 非模型参数的变量初始化为0和1
+        self.moving_mean = torch.zeros(shape)
+        self.moving_var = torch.ones(shape)
+
+    def forward(self, X):
+        # 如果X不在内存上，将moving_mean和moving_var
+        # 复制到X所在显存上
+        if self.moving_mean.device != X.device:
+            self.moving_mean = self.moving_mean.to(X.device)
+            self.moving_var = self.moving_var.to(X.device)
+        # 保存更新过的moving_mean和moving_var
+        Y, self.moving_mean, self.moving_var = batch_norm(
+            X, self.gamma, self.beta, self.moving_mean,
+            self.moving_var, eps=1e-5, momentum=0.9)
+        return Y
+
+def batch_norm(X, gamma, beta, moving_mean, moving_var, eps, momentum):
+    '''
+    批量规范化层
+    :param X: 输入数据，可以是一个二维张量（对于全连接层）或一个四维张量（对于卷积层）。
+    :param gamma:
+    :param beta: gamma、beta这两个是可学习的缩放和偏移参数，用于在归一化后对数据进行调整。
+    :param moving_mean: 移动平均的均值，用于在推理（或评估）阶段进行归一化。
+    :param moving_var: 移动平均的方差，用于在推理（或评估）阶段进行归一化。
+    :param eps: 一个小的正数，用于防止分母为零，确保数值稳定性。
+    :param momentum: 用于计算移动平均的动量值，通常接近1（如0.9或0.99）。
+    :return:
+    '''
+
+    # 通过is_grad_enabled来判断当前模式是训练模式还是预测模式
+    if not torch.is_grad_enabled():
+        # 如果是在预测模式下，直接使用传入的移动平均所得的均值和方差
+        X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
+    else:
+        assert len(X.shape) in (2, 4)
+        if len(X.shape) == 2:
+            # 使用全连接层的情况，计算特征维上的均值和方差
+            mean = X.mean(dim=0)
+            var = ((X - mean) ** 2).mean(dim=0)
+        else:
+            # 使用二维卷积层的情况，计算通道维上（axis=1）的均值和方差。
+            # 这里我们需要保持X的形状以便后面可以做广播运算
+            mean = X.mean(dim=(0, 2, 3), keepdim=True)
+            var = ((X - mean) ** 2).mean(dim=(0, 2, 3), keepdim=True)
+        # 训练模式下，用当前的均值和方差做标准化
+        X_hat = (X - mean) / torch.sqrt(var + eps)
+        # 更新移动平均的均值和方差
+        moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
+        moving_var = momentum * moving_var + (1.0 - momentum) * var
+    Y = gamma * X_hat + beta  # 缩放和移位
+    return Y, moving_mean.data, moving_var.data
--- d2l/code/main.py
+++ d2l/code/main.py
@@ -1,48 +1,117 @@
 import torch
-
+from torch import nn
 
 from train_util import train_ch3
 from train_util import train_ch6
 from data_util import load_data_fashion_mnist
 from gpu_util import try_gpu
-from cnn.cnn_model import lenet,alexnet
+from cnn.cnn_model import lenet,alexnet,vgg,nin,googlenet,BatchNorm
 
 
+def input_shape_change(X,net):
+    '''
+    查看输入经过模型的每层的变化
+    :param X: 输入
+    :param net: 模型
+    :return:
+    '''
+    for layer in net:
+        X = layer(X)
+        print(layer.__class__.__name__, 'output shape: \t', X.shape)
+
+def train_fashion_mnist(lr,num_epochs,batch_size,net):
+
+    train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
+    train_ch6(net, train_iter, test_iter, num_epochs, lr, try_gpu())
 
 def lenet_train():
     net = lenet()
 
     # 输入为28*28单通道
     X = torch.rand(size=(1, 1, 28, 28), dtype=torch.float32)
-    for layer in net:
-        X = layer(X)
-        print(layer.__class__.__name__, 'output shape: \t', X.shape)
+
+    input_shape_change(X,net)
 
     # 模型训练
     batch_size = 256
-    train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
 
     lr, num_epochs = 0.9, 10
-    train_ch6(net, train_iter, test_iter, num_epochs, lr, try_gpu())
+
+    train_fashion_mnist(lr,num_epochs,batch_size,net)
+
 
 def alexnet_train():
     net = alexnet()
     X = torch.randn(1, 1, 224, 224)
-    for layer in net:
-        X = layer(X)
-        print(layer.__class__.__name__, 'output shape:\t', X.shape)
+
+    input_shape_change(X, net)
 
     batch_size = 128
-    train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
-
     lr, num_epochs = 0.01, 10
-    train_ch6(net, train_iter, test_iter, num_epochs, lr, try_gpu())
+    train_fashion_mnist(lr, num_epochs, batch_size, net)
+
+
+def vgg_train():
+    conv_arch = ((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))
+    net = vgg(conv_arch)
+
+    X = torch.randn(size=(1, 1, 224, 224))
+
+    input_shape_change(X,net)
+
+    # VGG-11比AlexNet计算量更大，这里使用通道数较少的网络
+    ratio = 4
+    small_conv_arch = [(pair[0], pair[1] // ratio) for pair in conv_arch]
+    net = vgg(small_conv_arch)
+    lr, num_epochs, batch_size = 0.05, 10, 128
+    train_fashion_mnist(lr, num_epochs, batch_size, net)
+
+def nin_train():
+    net = nin()
+    X = torch.rand(size=(1, 1, 224, 224))
+    input_shape_change(X,net)
+    # 训练模型
+    lr, num_epochs, batch_size = 0.1, 10, 128
+    train_fashion_mnist(lr, num_epochs, batch_size, net)
+
+
+def googlenet_train():
+   net = googlenet()
+   X = torch.rand(size=(1, 1, 96, 96))
+   input_shape_change(X,net)
+
+   lr, num_epochs, batch_size = 0.1, 10, 128
+   train_fashion_mnist(lr, num_epochs, batch_size, net)
+
+def batch_norm_train():
+    # net = nn.Sequential(
+    #     nn.Conv2d(1, 6, kernel_size=5), BatchNorm(6, num_dims=4), nn.Sigmoid(),
+    #     nn.AvgPool2d(kernel_size=2, stride=2),
+    #     nn.Conv2d(6, 16, kernel_size=5), BatchNorm(16, num_dims=4), nn.Sigmoid(),
+    #     nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
+    #     nn.Linear(16 * 4 * 4, 120), BatchNorm(120, num_dims=2), nn.Sigmoid(),
+    #     nn.Linear(120, 84), BatchNorm(84, num_dims=2), nn.Sigmoid(),
+    #     nn.Linear(84, 10))
+    net = nn.Sequential(
+        nn.Conv2d(1, 6, kernel_size=5), nn.BatchNorm2d(6), nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2),
+        nn.Conv2d(6, 16, kernel_size=5), nn.BatchNorm2d(16), nn.Sigmoid(),
+        nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
+        nn.Linear(256, 120), nn.BatchNorm1d(120), nn.Sigmoid(),
+        nn.Linear(120, 84), nn.BatchNorm1d(84), nn.Sigmoid(),
+        nn.Linear(84, 10))
+    lr, num_epochs, batch_size = 1.0, 10, 256
+    train_fashion_mnist(lr, num_epochs, batch_size, net)
+
 
 if __name__ == '__main__':
 
     ######################################
     # cnn
     ######################################
-    lenet_train()
+    # lenet_train()
 
-    alexnet_train
+    # alexnet_train()
+
+    # googlenet_train()
+    batch_norm_train()

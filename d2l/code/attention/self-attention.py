584321c9a163c5fd54cdc1255e998b0b3626e555
LeoLi04 <leoli_4@163.com>
2024-06-30 23:09:48 +0800

    feat：transformer

---
+++ d2l/code/attention/self-attention.py
@@ -0,0 +1,30 @@
+import math
+import torch
+from torch import nn
+from d2l import torch as d2l
+from multi_head_attention_module import MultiHeadAttention
+
+
+class PositionalEncoding(nn.Module):
+    """位置编码"""
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        # 创建一个足够长的P
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arange(max_len, dtype=torch.float32).reshape(
+            -1, 1) / torch.pow(10000, torch.arange(
+            0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)
+        self.P[:, :, 1::2] = torch.cos(X)
+
+    def forward(self, X):
+        X = X + self.P[:, :X.shape[1], :].to(X.device)
+        return self.dropout(X)
+
+
+if __name__ == '__main__':
+    num_hiddens, num_heads = 100, 5
+    attention = MultiHeadAttention(num_hiddens, num_hiddens, num_hiddens,
+                                       num_hiddens, num_heads, 0.5)
+    print(attention.eval())

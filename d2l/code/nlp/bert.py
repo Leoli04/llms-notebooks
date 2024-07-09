9a13e5ec3800f26d70550d6e34568cc956017f0b
LeoLi04 <leoli_4@163.com>
2024-07-09 10:10:50 +0800

    feat：nlp bert

--- d2l/code/nlp/bert.py
+++ d2l/code/nlp/bert.py
@@ -9,12 +9,16 @@ def get_tokens_and_segments(tokens_a, tokens_b=None):
     """
     获取输入序列的词元及其片段索引
     将一个句子或两个句子作为输入，然后返回BERT输入序列的标记及其相应的片段索引
+    当输入为单个文本时，BERT输入序列是特殊类别词元“<cls>”、文本序列的标记、以及特殊分隔词元“<sep>”的连结。
+    当输入为文本对时，BERT输入序列是“<cls>”、第一个文本序列的标记、“<sep>”、第二个文本序列标记、以及“<sep>”的连结。
     """
     tokens = ['<cls>'] + tokens_a + ['<sep>']
     # 0和1分别标记片段A和B
+    # 初始化一个长度为len(tokens_a) + 2，元素全是0的数组
     segments = [0] * (len(tokens_a) + 2)
     if tokens_b is not None:
         tokens += tokens_b + ['<sep>']
+        # 对第二个文本初始化1
         segments += [1] * (len(tokens_b) + 1)
     return tokens, segments
 
@@ -45,13 +49,111 @@ class BERTEncoder(nn.Module):
             X = blk(X, valid_lens)
         return X
 
+#@save
+class MaskLM(nn.Module):
+    """
+    BERT的掩蔽语言模型任务
+    BERT随机掩蔽词元并使用来自双向上下文的词元以自监督的方式预测掩蔽词元
+    """
+    def __init__(self, vocab_size, num_hiddens, num_inputs=768, **kwargs):
+        super(MaskLM, self).__init__(**kwargs)
+        self.mlp = nn.Sequential(nn.Linear(num_inputs, num_hiddens),
+                                 nn.ReLU(),
+                                 nn.LayerNorm(num_hiddens),
+                                 nn.Linear(num_hiddens, vocab_size))
+
+    def forward(self, X, pred_positions):
+        num_pred_positions = pred_positions.shape[1]
+        pred_positions = pred_positions.reshape(-1)
+        batch_size = X.shape[0]
+        batch_idx = torch.arange(0, batch_size)
+        # 假设batch_size=2，num_pred_positions=3
+        # 那么batch_idx是np.array（[0,0,0,1,1,1]）
+        batch_idx = torch.repeat_interleave(batch_idx, num_pred_positions)
+        masked_X = X[batch_idx, pred_positions]
+        masked_X = masked_X.reshape((batch_size, num_pred_positions, -1))
+        mlm_Y_hat = self.mlp(masked_X)
+        return mlm_Y_hat
+
+#@save
+class NextSentencePred(nn.Module):
+    """BERT的下一句预测任务"""
+    def __init__(self, num_inputs, **kwargs):
+        super(NextSentencePred, self).__init__(**kwargs)
+        self.output = nn.Linear(num_inputs, 2)
+
+    def forward(self, X):
+        # X的形状：(batchsize,num_hiddens)
+        return self.output(X)
+
+#@save
+class BERTModel(nn.Module):
+    """
+    BERT模型
+    """
+    def __init__(self, vocab_size, num_hiddens, norm_shape, ffn_num_input,
+                 ffn_num_hiddens, num_heads, num_layers, dropout,
+                 max_len=1000, key_size=768, query_size=768, value_size=768,
+                 hid_in_features=768, mlm_in_features=768,
+                 nsp_in_features=768):
+        super(BERTModel, self).__init__()
+        self.encoder = BERTEncoder(vocab_size, num_hiddens, norm_shape,
+                    ffn_num_input, ffn_num_hiddens, num_heads, num_layers,
+                    dropout, max_len=max_len, key_size=key_size,
+                    query_size=query_size, value_size=value_size)
+        self.hidden = nn.Sequential(nn.Linear(hid_in_features, num_hiddens),
+                                    nn.Tanh())
+        self.mlm = MaskLM(vocab_size, num_hiddens, mlm_in_features)
+        self.nsp = NextSentencePred(nsp_in_features)
+
+    def forward(self, tokens, segments, valid_lens=None,
+                pred_positions=None):
+        encoded_X = self.encoder(tokens, segments, valid_lens)
+        if pred_positions is not None:
+            mlm_Y_hat = self.mlm(encoded_X, pred_positions)
+        else:
+            mlm_Y_hat = None
+        # 用于下一句预测的多层感知机分类器的隐藏层，0是“<cls>”标记的索引
+        nsp_Y_hat = self.nsp(self.hidden(encoded_X[:, 0, :]))
+        return encoded_X, mlm_Y_hat, nsp_Y_hat
+
 if __name__ == '__main__':
     vocab_size, num_hiddens, ffn_num_hiddens, num_heads = 10000, 768, 1024, 4
     norm_shape, ffn_num_input, num_layers, dropout = [768], 768, 2, 0.2
     encoder = BERTEncoder(vocab_size, num_hiddens, norm_shape, ffn_num_input,
                           ffn_num_hiddens, num_heads, num_layers, dropout)
 
+    # 将tokens定义为长度为8的2个输入序列，其中每个词元是词表的索引
     tokens = torch.randint(0, vocab_size, (2, 8))
     segments = torch.tensor([[0, 0, 0, 0, 1, 1, 1, 1], [0, 0, 0, 1, 1, 1, 1, 1]])
     encoded_X = encoder(tokens, segments, None)
     print(encoded_X.shape)
+
+    ############################
+    # 掩蔽语言模型
+    ############################
+
+    mlm = MaskLM(vocab_size, num_hiddens)
+    mlm_positions = torch.tensor([[1, 5, 2], [6, 1, 5]])
+    mlm_Y_hat = mlm(encoded_X, mlm_positions)
+    print(mlm_Y_hat.shape)
+
+    mlm_Y = torch.tensor([[7, 8, 9], [10, 20, 30]])
+    loss = nn.CrossEntropyLoss(reduction='none')
+    mlm_l = loss(mlm_Y_hat.reshape((-1, vocab_size)), mlm_Y.reshape(-1))
+    print(mlm_l.shape)
+
+    ############################
+    # 下一句预测
+    ############################
+
+    encoded_X = torch.flatten(encoded_X, start_dim=1)
+    # NSP的输入形状:(batchsize，num_hiddens)
+    nsp = NextSentencePred(encoded_X.shape[-1])
+    nsp_Y_hat = nsp(encoded_X)
+    print(nsp_Y_hat.shape)
+
+    # 计算两个二元分类的交叉熵损失
+    nsp_y = torch.tensor([0, 1])
+    nsp_l = loss(nsp_Y_hat, nsp_y)
+    print(nsp_l.shape)
